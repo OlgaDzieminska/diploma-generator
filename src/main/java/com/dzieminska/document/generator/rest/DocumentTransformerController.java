c4849ee333201937951e65e4d089a4cf98989e96
Olga Dziemińska <olgadziem@gmail.com>
2023-05-07 20:16:55 +0200

    Refactor DocumentTransformerController

--- src/main/java/com/dzieminska/document/generator/rest/DocumentTransformerController.java
+++ src/main/java/com/dzieminska/document/generator/rest/DocumentTransformerController.java
@@ -1,19 +1,21 @@
 package com.dzieminska.document.generator.rest;
+
 import java.io.IOException;
 import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.apache.commons.io.IOUtils;
 
 @RestController
 @RequestMapping("/diploma")
 public class DocumentTransformerController {
 
-	@GetMapping(value = "/get-file",produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	public byte[] getFile() throws IOException{
-		InputStream in=getClass().getResourceAsStream("/DiplomaGenerator.py");
+	@GetMapping(value = "/generate", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public byte[] generateFile() throws IOException {
+		InputStream in = getClass().getResourceAsStream("/DiplomaGenerator.py");
 		return IOUtils.toByteArray(in);
 	}
 }
